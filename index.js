a01eddf8dcba10bf3e1dbbd59a64d4571a15d568
jvpink200 <jvillarr20@gmail.com>
2018-08-23 01:46:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var katzDeli = [];
+var takeANumber = function(katzDeli, newCustomer) {
+
+ if (katzDeli.length > 0);
+     katzDeli.push(newCustomer);
+     return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
+  }
