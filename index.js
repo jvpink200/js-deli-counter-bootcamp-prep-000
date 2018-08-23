a3e785cc602c695d11fc154b254b32d65dc7a39b
jvpink200 <jvillarr20@gmail.com>
2018-08-23 01:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ var takeANumber = function(katzDeli, newCustomer) {
      katzDeli.push(newCustomer);
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
+
+
+  function currentLine(arr) {
+    var empty = [];
+    if (arr.length < 1) {
+      return 'There is nobody in line.';
+    } else {
+      for (var i = 0; i < arr.length; i++) {
+        empty.push(arr.indexOf(arr[i]) + 1, '.', arr[i], ', ');
+      }
+      return 'The line is currently: ' + empty.join(' ');
+    }
+  }
+  currentLine(katzDeliLine); // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+  
+  function nowServing(arr) {
+    var firstPerson = [];
+    if (arr.length < 1) {
+      return 'There is nobody waiting to be served!';
+    } else {
+      firstPerson.push(arr[0]);
+      arr.shift();
+      return 'Currently serving ' + firstPerson[0] + '.';
+    }
+  }
